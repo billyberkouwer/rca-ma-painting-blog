5a3e0c6333dc050bef5a8426906f7d64ba72f1ea
billy.berkouwer <billy@ocove.studio>
2025-09-05 13:42:17 +0100

    fix: sanity revalidation

--- src/app/api/revalidate-path/route.ts
+++ src/app/api/revalidate-path/route.ts
@@ -20,10 +20,8 @@ export async function POST(req: NextRequest) {
         if (!isValidSignature) {
             const message = 'Invalid signature'
             return new Response(JSON.stringify({ message, isValidSignature, body }), { status: 401 })
-        } else if (!body?.slug) {
-            const message = 'Bad Request'
-            return new Response(JSON.stringify({ message, body }), { status: 400 })
         }
+        
         console.log("Revalidating path");
         revalidatePath("/");
 
