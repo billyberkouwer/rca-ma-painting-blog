22409cf344cf609ed35a40dccd93415c470ad916
billy.berkouwer <billy@ocove.studio>
2025-09-05 12:09:30 +0100

    update: remove draftmode

--- src/app/api/draft-mode/enable/route.ts
+++
@@ -1,9 +0,0 @@
-// src/app/api/draft-mode/enable/route.ts
-
-import { token } from '@/sanity/env'
-import { client } from '@/sanity/lib/client'
-import { defineEnableDraftMode } from 'next-sanity/draft-mode'
-
-export const { GET } = defineEnableDraftMode({
-    client: client.withConfig({ token }),
-})
