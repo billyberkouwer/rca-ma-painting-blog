4f53bb47ec8436b6a30f2170052009a980a93a65
billy.berkouwer <billy@ocove.studio>
2025-09-10 14:08:41 +0100

    update: remove unused sanity elements

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -14,43 +14,8 @@ const IndexDocument = defineType({
       title: "Title",
     }),
     defineField({
-      name: "elements",
-      title: "Elements",
-      type: "array",
-      of: [
-        {
-          type: "reference",
-          to: [{ type: "imageBlock" }, { type: "textBlock" }],
-        },
-      ],
-    }),
-    defineField({
       name: "pageMeta",
       type: "pageMeta",
-    })
-  ]
-})
-
-export const ImageType = defineType({
-  type: "document",
-  name: "imageBlock",
-  title: "Image",
-  fields: [
-    defineField({
-      type: "image",
-      name: "image",
-      title: "Image",
-    }),
-    defineField({
-      type: "string",
-      name: "alt",
-      title: "Alt",
-    }),
-    defineField({
-      type: "array",
-      of: [{ type: "block" }],
-      name: "caption",
-      title: "Caption",
     })
   ]
 })
@@ -80,25 +45,6 @@ export const ImageArrayType = defineType({
   ]
 })
 
-export const TextType = defineType({
-  type: "document",
-  name: "textBlock",
-  title: "Text",
-  fields: [
-    defineField({
-      type: "string",
-      name: "title",
-      title: "Title",
-    }),
-    defineField({
-      type: "array",
-      name: "text",
-      of: [{ type: "block" }],
-      title: "Text",
-    })
-  ]
-})
-
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [IndexDocument, TextType, ImageType, ImageArrayType, pageMeta, postsSchema, aboutSchema],
+  types: [IndexDocument, ImageArrayType, pageMeta, postsSchema, aboutSchema],
 }
